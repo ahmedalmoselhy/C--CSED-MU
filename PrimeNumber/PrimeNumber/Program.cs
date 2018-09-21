cc2d518c6d97bef3484788ad608cab71c636d2d3
Ahmed Hani <ahmedalmoselhy.slm@gmail.com>
2018-09-21 13:52:29 +0200

    Adding more projects

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int i, j, k, l;
+
+            Console.Write("Enter A Number : ");
+            x = int.Parse(Console.ReadLine());
+
+            if (x == 2 || x == 3 || x == 5 || x == 7)
+            {
+                Console.WriteLine("It's Primal");
+
+            }
+            else
+            {
+                i = x % 2;
+                j = x % 3;
+                k = x % 5;
+                l = x % 7;
+
+
+                if (i != 0 && j != 0 && k != 0 && l != 0)
+                {
+                    Console.WriteLine("It's Primal!");
+                }
+                else
+                {
+                    Console.WriteLine("It's NOT Primal");
+                }
+            }
+        }
+    }
+}
---
+++ ReverseAString/ReverseAString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            Console.Write("Enter A Word : ");
+            word = Console.ReadLine();
+
+            int x = 0;
+
+            for (x = 0; x < word.Length; x++)
+            {
+                Console.Write(word[word.Length - 1 - x]);
+
+            }
+            Console.WriteLine("");
+        }
+    }
+}
